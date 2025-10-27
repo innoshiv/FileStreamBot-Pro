14538d88534317de1c8022631e60c1d16dc8b1fc
Shivam Kumar <shivamnox0@gmail.com>
2025-10-27 14:10:14 +0530

    Update vars.py

--- ShivamNox/vars.py
+++ ShivamNox/vars.py
@@ -9,20 +9,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '28405636'))
-    API_HASH = str(getenv('API_HASH', '52b6dfbaeea988407006cefbea7c2e04'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '7902407945:AAFCJidyZ1ELUnQuR41iv6QJkslc8j9SzRI'))
-    name = str(getenv('name', 'FileStreamBot'))
+    API_ID = int(getenv('API_ID', '28405636'))  #Required
+    API_HASH = str(getenv('API_HASH', '52b6dfbaeea988407006cefbea7c2e04')) #Required
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '7902407945:AAFCJidyZ1ELUnQuR41iv6QJkslc8j9SzRI')) #Required
+    name = str(getenv('name', 'FileStreamBot')) #Optional
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002044705664'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002044705664')) #Required
     PORT = int(getenv('PORT', '3000'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6713397633").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6713397633").split())  #Required
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'shivamnox'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'shivamnox')) #Required
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://filestreambot-pro-js53.onrender.com/".format(FQDN)
     else:
         URL = "https://filestreambot-pro-js53.onrender.com/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://daxonultra:QPRXnQLz9sCr1K8l@cluster0.3mqoc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'hivajoyweb'))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://daxonultra:QPRXnQLz9sCr1K8l@cluster0.3mqoc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')) #Required
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'hivajoyweb')) #Required
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) #Leave as it is.
